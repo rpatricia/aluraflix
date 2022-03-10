ea9a7b1e98c9702e1df39260d1ddbfb645685139
Renata Pausini <86075683+rpatricia@users.noreply.github.com>
2022-03-10 12:08:49 -0300

    Adicionando o Alura Flix

---
+++ script.js
@@ -0,0 +1,9 @@
+var listaFilmes = [
+  "https://i.pinimg.com/originals/2f/9d/7f/2f9d7fc768f2861850a87cb33a1b1455.jpg ",
+  " https://www.papeldeparede.etc.br/fotos/wp-content/uploads/star-wars--a-forca_6523_1024x768.jpg",
+  "https://data.1freewallpapers.com/download/new-amazing-spider-man-1440x1080.jpg "
+];
+
+for (var i = 0; i < listaFilmes.length; i++) {
+  document.write("<img src=" + listaFilmes[i] + ">");
+}
